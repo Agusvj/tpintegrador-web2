b8890b2f75132a52c3599d3ea91615a0b94e7bf7
AgusVJ <agustinvillanuevajousset@gmail.com>
2025-09-18 19:24:47 -0300

    agregado categories.js para llamado a la api por separado dentro de data

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,5 +1,5 @@
-export async function getProducts() {
-  fetch("http://161.35.104.211:8000/products", {
+export async function getCategories() {
+  fetch("http://161.35.104.211:8000/categories", {
     headers: {
       accept: "application/json",
       Authorization: "Bearer jeremias01",
@@ -9,8 +9,8 @@ export async function getProducts() {
     .then((res) => console.log(res));
 }
 
-export async function getProductByID(id) {
-  fetch(`http://161.35.104.211:8000/products/${id}`, {
+export async function getCategorieByID(id) {
+  fetch(`http://161.35.104.211:8000/categories/${id}`, {
     headers: {
       accept: "application/json",
       Authorization: "Bearer jeremias01",
