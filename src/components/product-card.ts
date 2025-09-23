b9c95458d9cd99fc3a40000fec338c10643a0e70
Topsy <abigailgomez94@gmail.com>
2025-09-22 22:43:19 -0300

    cambios en product card

---
+++ src/components/product-card.ts
@@ -0,0 +1,7 @@
+import{LitElement,html,css}from 'lit';
+import { property } from 'lit/decorators.js';
+//equivale a colocar CustomElements.define() en JS
+@customElement ('product-card')
+class ProductCard extends LitElement{
+    @property
+}
