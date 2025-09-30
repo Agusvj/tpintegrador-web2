6ce669e23c8e48ca38928fb8b042b259b4a005fc
Crisspany <andreapaniagua157@gmail.com>
2025-09-30 15:31:55 -0300

    responsive en bt tags

--- src/components/tag-list.ts
+++ src/components/tag-list.ts
@@ -15,39 +15,40 @@ export class Tags_List extends LitElement {
     }
 
     return html`
-      <section class=" flex  flex-wrap items-center justify-center">
-        <div
-          class=" mx-auto max-w-screen-xl px-2 py-4 sm:px-6 sm:py-12 lg:px-8 "
-        >
-          <header class="inline">
+    
+      <section>
+      <header>
             <h2
               class="text-xl text-center  py-8 font-bold text-gray-900 sm:text-3xl"
             >
               Buscá por Tags
             </h2>
           </header>
+        <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 grid grid-cols-2 gap-4  sm:grid-cols-4 justify-items-center ">
+          
           ${this.tags.map((tag) => {
             return html`
          
               <a
-                class="inline-block rounded-sm bg-blue-800 px-8  py-2 mx-5 text-sm   font-medium text-white transition hover:scale-110 hover:shadow-xl focus:ring-3 focus:outline-hidden"
+                class=" w-40 rounded-sm bg-blue-800  py-2  text-sm  text-center font-medium text-white transition hover:scale-110 hover:shadow-xl focus:ring-3 focus:outline-hidden"
                 href="http://localhost:5173/index.html?tag=${tag.id}"
               >
                 ${tag.title}
               </a>
               
-            </div>
-          </section>
+              
+            
+      
         `;
           })}
-          <a
-                class="inline-block rounded-sm bg-blue-800 px-8  py-2 mx-5 text-sm   font-medium text-white transition hover:scale-110 hover:shadow-xl focus:ring-3 focus:outline-hidden"
+          
+          <a class="text-center w-40 rounded-sm bg-blue-800   py-2  text-sm   font-medium text-white transition hover:scale-110 hover:shadow-xl focus:ring-3 focus:outline-hidden"
                 href="http://localhost:5173/index.html"
               >
                 Mostrar Todo
               </a>
         </div>
       </section>
-    </div> `;
+     `;
   }
 }
