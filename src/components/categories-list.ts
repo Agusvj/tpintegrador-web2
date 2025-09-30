e66ee5579dbbd84d93e4587787fe72b4259fe7b1
Crisspany <andreapaniagua157@gmail.com>
2025-09-30 11:27:31 -0300

    cambios en botones de categorias

--- src/components/categories-list.ts
+++ src/components/categories-list.ts
@@ -13,19 +13,20 @@ export class Categories_List extends LitElement {
   render() {
     return html`
       <section>
-        <div class=" mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8  ">
-          <header>
+      <header>
             <h2
               class="text-xl text-center py-8 font-bold text-gray-900 sm:text-3xl"
             >
               Buscá por categorias
             </h2>
           </header>
+        <div class=" mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 grid grid-cols-2 gap-4 sm:grid-cols-4 justify-items-center ">
+          
           ${this.categorias.map((cat) => {
             return html`
             
               <a
-                class="inline-block rounded-sm bg-blue-800 px-8  py-2 mx-5 text-sm   font-medium text-white transition hover:scale-110 hover:shadow-xl focus:ring-3 focus:outline-hidden"
+                class=" rounded-sm bg-blue-800 px-8  w-40 py-2 text-sm  text-center font-medium text-white transition hover:scale-110 hover:shadow-xl focus:ring-3 focus:outline-hidden"
                 href="http://localhost:5173/listado.html?cat=${cat.id}"
               >
                 ${cat.title}
